b9415b3b088b5d33d90353e89d8dbc5974778bd9
Dongukkk <kdu0816@gmail.com>
2025-05-09 14:03:11 +0900

    매개변수, 리턴타입 다형성

---
+++ src/study/inherit/inherit06/Driver.java
@@ -0,0 +1,18 @@
+package study.inherit.inherit06;
+
+public class Driver {
+//	public void drive(Bus bus) {
+//		System.out.println("Driver 운전 시작");
+//		bus.run();
+//	}
+//	
+//	public void drive(Taxi taxi) {
+//		System.out.println("Driver 운전 시작");
+//		taxi.run();
+//	}
+	
+	public void drive(Vehicle vehicle) {
+		System.out.println("Driver 운전 시작");
+		vehicle.run();
+	}
+}
